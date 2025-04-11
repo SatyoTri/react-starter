adabb66e8c11694e39ca8250941c725b9d4e0737
SatyoTri <satyohanggoro@gmail.com>
2025-04-11 19:28:14 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
